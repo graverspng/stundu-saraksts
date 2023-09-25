5687a01755be400205392e1222fa8fe2fc715947
Skolnieks <skolnieks.pjvt@pjvt.local>
2023-09-25 09:37:49 +0300

    stundu saraksts

--- src/App.js
+++ src/App.js
@@ -4,21 +4,37 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <table class="tabula">
+      <tbody>
+        <tr class="tr1">
+          <td>
+            <span class="info2">Pārst. </span>
+              <a>Pirmdiena,  </a>
+              <a>2023</a>
+              <small>.gada </small>
+              <b>25.septembris</b>
+              <span class="nedruka">
+                <span slass="nedruka"> [šodien]</span>
+              </span>
+          </td>
+        </tr>
+        <tr>
+          <td valign="top">
+            <table class="tabula2">
+            <tbody>
+              <tr class="stunda1">
+                <td rowspan="2" valign="middle"></td>
+                <td width="60%" class="txm">Sistēmu programmēšana</td>
+                <td width="35%" class="txn">C.203-J.Krīgerts</td>
+              </tr>
+            </tbody>
+            </table>
+          </td>
+        </tr>
+      </tbody>
+        
+    </table>
+</div>
   );
 }
 
